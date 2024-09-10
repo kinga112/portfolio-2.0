624c56565eb3960e8ceaed6659f2411615adeca0
kinga112 <kinga112@yahoo.com>
2024-09-10 17:12:22 -0400

    top padding got moved to margin on title wrapper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,8 @@ import Hobbies from "../components/Home/Hobbies"
 function Home(){
   return(
     <>
-      <div className='pt-28 h-full w-full p-2 sm:p-10 space-y-5 overflow-hidden'>
-        <div className='flex h-[500px] w-full bg-custom rounded-lg overflow-hidden justify-center place-items-center'>
+      <div className=' h-full w-full p-10 sm:p-10 space-y-5 overflow-hidden'>
+        <div className='flex h-[500px] w-full bg-custom rounded-lg overflow-hidden justify-center place-items-center mt-10'>
           <Title/>
           <ImageGrid/>
         </div>
