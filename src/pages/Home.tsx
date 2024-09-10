e3d1e67ecfa56e3ffd485d87ccb3c4837685a9ea
kinga112 <kinga112@yahoo.com>
2024-09-10 17:15:04 -0400

    removed test padding title page (back to 2)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ import Hobbies from "../components/Home/Hobbies"
 function Home(){
   return(
     <>
-      <div className=' h-full w-full p-10 sm:p-10 space-y-5 overflow-hidden'>
+      <div className=' h-full w-full p-2 sm:p-10 space-y-5 overflow-hidden'>
         <div className='flex h-[500px] w-full bg-custom rounded-lg overflow-hidden justify-center place-items-center mt-10'>
           <Title/>
           <ImageGrid/>
