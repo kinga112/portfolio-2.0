6be65d9766e614ee688d06bde53809c0d46a36ac
Alden King <kinga112@yahoo.com>
2026-07-17 12:42:10 -0400

    commented out unused portions of Travel page. Updates coming soon.

--- src/pages/Travel.tsx
+++ src/pages/Travel.tsx
@@ -1,14 +1,3 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import ba from '../assets/image-grid/ba.jpg'
-import volcanoTree from '../assets/image-grid/volcanoTree.jpg'
-import dogSledPlace from '../assets/image-grid/dogSledPlace.jpg'
-import glacier from '../assets/image-grid/glacier.jpg'
-import grafiti from '../assets/image-grid/grafiti.jpg'
-import grogu from '../assets/image-grid/grogu.jpg'
-
 function Travel(){
   return(
     <>
@@ -63,68 +52,26 @@ function Travel(){
 
 export default Travel
 
-interface TravelLocationProps {
-  location: string;
-  date: string;
-  children?: React.ReactNode;
-}
+// interface TravelLocationProps {
+//   location: string;
+//   date: string;
+//   children?: React.ReactNode;
+// }
 
-function TravelLocation({location, date, children}: TravelLocationProps) {
-  return (
-    <>
-      <div className="flex flex-col gap-3">
-        <div className="flex gap-5 place-items-center">
-          <div className="text-3xl font-light">
-            {location}
-          </div>
-          <div className="text-sm">
-            {date}
-          </div>
-        </div>
-        {children}
-      </div>
-    </>
-  )
-}
-
-function PictureCarousel(){
-  // props: {images: Array<string>}
-  // const images = [9, 8, 7, 6, 5].map((number) => ({
-  //   src: `https://placedog.net/${number}00/${number}00?id=${number}`
-  // }));
-  return(
-    <>
-        <Carousel
-          // showArrows={true}
-          infiniteLoop
-          autoPlay={true}
-          width={150}
-          dynamicHeight={false}
-          // emulateTouch
-          // onClickItem={(...args) => console.log('onClickItem', ...args)}
-          // onChange={(...args) => console.log('onChange', ...args)}
-          // onClickThumb={(...args) => console.log('onClickThumb', ...args)}
-          >
-          <div className="flex place-items-center h-44">
-            <img className="object-contain" src={ba}/>
-            {/* <p className="legend">Legend 1</p> */}
-          </div>
-          <div className="flex place-items-center h-44">
-            <img className="object-contain" src={glacier}/>
-          </div>
-          <div className="flex place-items-center h-44">
-            <img className="object-contain" src={dogSledPlace}/>
-          </div>
-          <div className="flex place-items-center h-44">
-            <img className="object-contain" src={grafiti}/>
-          </div>
-          <div className="flex place-items-center h-44">
-            <img className="object-contain" src={grogu}/>
-          </div>
-          <div className="flex place-items-center h-44">
-            <img className="object-contain" src={volcanoTree}/>
-          </div>
-        </Carousel>
-    </>
-  )
-}
+// function TravelLocation({location, date, children}: TravelLocationProps) {
+//   return (
+//     <>
+//       <div className="flex flex-col gap-3">
+//         <div className="flex gap-5 place-items-center">
+//           <div className="text-3xl font-light">
+//             {location}
+//           </div>
+//           <div className="text-sm">
+//             {date}
+//           </div>
+//         </div>
+//         {children}
+//       </div>
+//     </>
+//   )
+// }
