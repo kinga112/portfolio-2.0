5a292e8b46d7789906cd32090d93deb96a600202
Alden King <kinga112@yahoo.com>
2026-07-17 10:06:36 -0400

    refactor: update mission page layout and styling

--- src/pages/Mission.tsx
+++ src/pages/Mission.tsx
@@ -6,49 +6,49 @@ import working from '../assets/mission/working.jpg'
 function Mission(){
   return(
     <>
-      <div className='pt-28 h-full w-full p-10 space-y-10'>
-        <div className='flex justify-center text-5xl font-extralight'>
-          Mission
+      <div className='h-full w-full p-10 space-y-10 bg-custom-forest-green text-custom-off-white'>
+        <div className='flex justify-center text-7xl font-light'>
+          My Mission
         </div>
-        <div className='flex flow-row text-2xl px-24'>
-          <img className='rounded-3xl shadow-xl' src={houseKids} height={450} width={500}/>
-          <div className='p-14'>
-            I am deeply committed to dedicating my time to helping others and making a positive impact on their lives. 
-            Inspired by my experiences volunteering on mission trips in Costa Rica and at a local soup kitchen, 
-            I believe in the transformative power of service. Through my work, I aim to contribute meaningfully to communities, 
-            fostering empathy, and creating lasting change. Each endeavor I undertake is driven by a profound belief in the 
+        <div className='flex flow-row text-2xl px-10 justify-around gap-10'>
+          <img className='rounded-2xl' src={houseKids} height={450} width={500}/>
+          <div className='p-14 bg-custom-light-green rounded-2xl'>
+            I am deeply committed to dedicating my time to helping others and making a positive impact on their lives.
+            Inspired by my experiences volunteering on mission trips in Costa Rica and at a local soup kitchen,
+            I believe in the transformative power of service. Through my work, I aim to contribute meaningfully to communities,
+            fostering empathy, and creating lasting change. Each endeavor I undertake is driven by a profound belief in the
             importance of compassion and action in building a better world.
           </div>
         </div>
-        <div className='flex flow-row text-2xl px-24'>
-          <div className='p-14'>
-            I am deeply committed to dedicating my time to helping others and making a positive impact on their lives. 
-            Inspired by my experiences volunteering on mission trips in Costa Rica and at a local soup kitchen, 
-            I believe in the transformative power of service. Through my work, I aim to contribute meaningfully to communities, 
-            fostering empathy, and creating lasting change. Each endeavor I undertake is driven by a profound belief in the 
+        <div className='flex flow-row text-2xl px-10 justify-around gap-10'>
+          <div className='p-14 bg-custom-light-green rounded-2xl'>
+            I am deeply committed to dedicating my time to helping others and making a positive impact on their lives.
+            Inspired by my experiences volunteering on mission trips in Costa Rica and at a local soup kitchen,
+            I believe in the transformative power of service. Through my work, I aim to contribute meaningfully to communities,
+            fostering empathy, and creating lasting change. Each endeavor I undertake is driven by a profound belief in the
             importance of compassion and action in building a better world.
           </div>
-          <img className='rounded-3xl shadow-xl' src={working} height={500} width={500}/>
+          <img className='rounded-2xl' src={working} height={500} width={500}/>
         </div>
-        <div className='flex flow-row text-2xl px-24'>
-          <img className='rounded-3xl shadow-xl' src={losGuitosKids} height={500} width={500}/>
-          <div className='p-14'>
-            I am deeply committed to dedicating my time to helping others and making a positive impact on their lives. 
-            Inspired by my experiences volunteering on mission trips in Costa Rica and at a local soup kitchen, 
-            I believe in the transformative power of service. Through my work, I aim to contribute meaningfully to communities, 
-            fostering empathy, and creating lasting change. Each endeavor I undertake is driven by a profound belief in the 
+        <div className='flex flow-row text-2xl px-10 justify-around gap-10'>
+          <img className='rounded-2xl' src={losGuitosKids} height={500} width={500}/>
+          <div className='p-14 bg-custom-light-green rounded-2xl'>
+            I am deeply committed to dedicating my time to helping others and making a positive impact on their lives.
+            Inspired by my experiences volunteering on mission trips in Costa Rica and at a local soup kitchen,
+            I believe in the transformative power of service. Through my work, I aim to contribute meaningfully to communities,
+            fostering empathy, and creating lasting change. Each endeavor I undertake is driven by a profound belief in the
             importance of compassion and action in building a better world.
           </div>
         </div>
-        <div className='flex flow-row text-2xl px-24'>
-          <div className='p-14'>
-            I am deeply committed to dedicating my time to helping others and making a positive impact on their lives. 
-            Inspired by my experiences volunteering on mission trips in Costa Rica and at a local soup kitchen, 
-            I believe in the transformative power of service. Through my work, I aim to contribute meaningfully to communities, 
-            fostering empathy, and creating lasting change. Each endeavor I undertake is driven by a profound belief in the 
+        <div className='flex flow-row text-2xl px-10 justify-around gap-10'>
+          <div className='p-14 bg-custom-light-green rounded-2xl'>
+            I am deeply committed to dedicating my time to helping others and making a positive impact on their lives.
+            Inspired by my experiences volunteering on mission trips in Costa Rica and at a local soup kitchen,
+            I believe in the transformative power of service. Through my work, I aim to contribute meaningfully to communities,
+            fostering empathy, and creating lasting change. Each endeavor I undertake is driven by a profound belief in the
             importance of compassion and action in building a better world.
           </div>
-          <img className='rounded-3xl shadow-xl' src={kid} height={350} width={350}/>
+          <img className='rounded-2xl' src={kid} height={350} width={350}/>
         </div>
       </div>
     </>
