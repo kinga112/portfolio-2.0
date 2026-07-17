107b452add121b64466f096f4ec8cd4d36db6a39
Alden King <kinga112@yahoo.com>
2026-07-17 15:02:36 -0400

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/next';
 import Home from "./pages/Home";
 import Music from "./pages/Music";
 import Mission from "./pages/Mission";
@@ -29,6 +30,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   );
 }
