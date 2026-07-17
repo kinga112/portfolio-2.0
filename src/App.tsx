b6b8ee4efb2f3321976fb1113d1c779ae60432cb
Alden King <kinga112@yahoo.com>
2026-07-17 15:10:42 -0400

    changed vercel analytics path to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import Home from "./pages/Home";
 import Music from "./pages/Music";
 import Mission from "./pages/Mission";
